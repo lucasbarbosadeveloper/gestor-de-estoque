c1f9153c8086bdc2f70f3d26ddf1ca08a08b638d
lucasbarbosadeveloper <lucasbarbosa20k@gmail.com>
2023-11-23 18:36:04 -0300

    feat: Home HTML finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import DeafaultStockLayout from "./components/DefaultStockLayout"
 import AllItems from "./pages/AllItems"
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <AllItems />
+    <Home />
   )
 
 }
--- src/pages/AllItems/index.jsx
+++ src/pages/AllItems/index.jsx
@@ -22,7 +22,6 @@ export default function AllItems() {
                     </td>
                 </tr>
 
-                
             </table>
         </>
     )
