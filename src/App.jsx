9e936af3b591357c6aa6dd9b7b5f2977c1635ae4
lucasbarbosadeveloper <lucasbarbosa20k@gmail.com>
2023-11-24 17:58:05 -0300

    feat: Item HTML finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import DeafaultStockLayout from "./components/DefaultStockLayout"
 import AllItems from "./pages/AllItems"
 import Home from "./pages/Home"
+import Item from "./pages/Item"
 
 function App() {
   return (
-    <Home />
+    <Item />
   )
 
 }
---
+++ src/pages/Item/index.jsx
@@ -0,0 +1,24 @@
+export default function Item() {
+    return (
+        <>
+            <div className="product">
+                <h2>7 Wonders</h2>
+                <button className="btn btn_update">Atualizar</button>
+                <button className="btn btn_del">Excluir</button>
+            </div>
+
+            <div className="infosCard">
+                <div>Categoria: Jogos</div>
+                <div>Quantidade em estoque: 8</div>
+                <div>Preço: R$ 399.99</div>
+            </div>
+
+            <p>Jogo de tabuleiro para vários jogadores</p>
+
+            <div className="updateDate">
+                <span>Cadastrado em: Fri Jun 16 2023</span>
+                <span>Atualizado em Fri Jun 16 2023</span>
+            </div>
+        </>
+    )
+}
