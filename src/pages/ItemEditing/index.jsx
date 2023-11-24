c5fb1639fd4c1381b4c60ef7eaf057c0654f75a7
lucasbarbosadeveloper <lucasbarbosa20k@gmail.com>
2023-11-24 19:17:12 -0300

    style: itemEditing stylization finished

--- src/pages/ItemEditing/index.jsx
+++ src/pages/ItemEditing/index.jsx
@@ -5,32 +5,35 @@ export default function ItemEditing() {
 
             <form className="cForm">
                 <div className="cform__inputs">
-                    <div className="inputCards">
+                    <div className="inputCards input1">
                         <label htmlFor="name">Nome</label>
                         <input type="text" name="name" id="name" />
                     </div>
 
-                    <div className="inputCards">
+                    <div className="inputCards input2">
                         <label htmlFor="quantity">Quantidade</label>
                         <input type="number" name="quantity" id="quantity" />
                     </div>
 
-                    <div className="inputCards">
+                    <div className="inputCards input3">
                         <label htmlFor="price">Preço</label>
                         <input type="number" name="price" id="price" />
                     </div>
 
-                    <div className="inputCards">
+                    <div className="inputCards  input4">
                         <label htmlFor="category">Categoria</label>
                         <select name="category" id="category">
                             <option value="Jogos">Jogos</option>
                             <option value="Hardware">Hardware</option>
                         </select>
                     </div>
+
+                    <div className="inputCards textAreaInput">
+                        <label htmlFor="description">Descrição</label>
+                        <textarea name="description" id="description" rows="5"></textarea>
+                    </div>
                 </div>
 
-                <label htmlFor="description">Descrição</label>
-                <textarea name="description" id="description" rows="10"></textarea>
 
                 <button type="submit" className="btn btn_view">Salvar</button>
             </form>
