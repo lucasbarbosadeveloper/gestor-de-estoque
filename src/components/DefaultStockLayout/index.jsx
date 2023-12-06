d8b33b386038a148e8645cde8db7f40732b3f7b6
lucasbarbosadeveloper <lucasbarbosa20k@gmail.com>
2023-12-06 15:40:29 -0300

    feat: Links and Outlat configured

--- src/components/DefaultStockLayout/index.jsx
+++ src/components/DefaultStockLayout/index.jsx
@@ -1,14 +1,16 @@
+import { Link, Outlet } from "react-router-dom";
+
 export default function DeafaultStockLayout() {
     return (
         <>
             <h1 className="title">Stock Items</h1>
             <div className="pages">
-                <a href="#">Todos os Itens</a>
-                <a href="#">Novo Item</a>
+                <Link to={"/stockItems"}>Todos os Itens</Link>
+                <Link to={"/stockItems/newItem"}>Novo Item</Link>
             </div>
 
             <div>
-
+                <Outlet />
             </div>
         </>
     )
