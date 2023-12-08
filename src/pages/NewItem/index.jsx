64d1c386fa85fbe68b228390050e6a0edd0a778c
lucasbarbosadeveloper <lucasbarbosa20k@gmail.com>
2023-12-08 10:44:48 -0300

    feat: newItem structure finalized

--- src/pages/NewItem/index.jsx
+++ src/pages/NewItem/index.jsx
@@ -1,11 +1,44 @@
-import ItemEditing from "../ItemEditing";
-
 export default function NewItem() {
+    
     return (
         <>
-           <ItemEditing
-                title="Novo item"
-           />
+            <h2>Novo Item</h2>
+
+            <form className="cForm">
+                <div className="cform__inputs">
+                    <div className="inputCards input1">
+                        <label htmlFor="name">Nome</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+
+                    <div className="inputCards input2">
+                        <label htmlFor="quantity">Quantidade</label>
+                        <input type="number" name="quantity" id="quantity" />
+                    </div>
+
+                    <div className="inputCards input3">
+                        <label htmlFor="price">Preço</label>
+                        <input type="number" name="price" id="price" />
+                    </div>
+
+                    <div className="inputCards  input4">
+                        <label htmlFor="category">Categoria</label>
+                        <select name="category" id="category">
+                            <option value="">--Selecione uma categoria--</option>
+                            <option value="Jogos">Jogos</option>
+                            <option value="Hardware">Hardware</option>
+                        </select>
+                    </div>
+
+                    <div className="inputCards textAreaInput">
+                        <label htmlFor="description">Descrição</label>
+                        <textarea name="description" id="description" rows="5"></textarea>
+                    </div>
+                </div>
+
+
+                <button type="submit" className="btn btn_view">Salvar</button>
+            </form>
         </>
     )
 }
